1b358d5cffe8f27fb366914ceab40373c01585e4
Veronica Stocco <veviz@hotmail.com>
2019-07-19 09:41:03 -0300

    ST working

--- EP16/WordNet.java
+++ EP16/WordNet.java
@@ -1,17 +1,47 @@
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.ST;
 
 public class WordNet {
+   // classe para guardar o valor lido de synsets
+   public class Word {
+      String synset;
+      String gloss;
+      Bag<Integer> hypernyms;
+
+
+      public Word (String synset, String gloss) {
+         if (synset == null || gloss == null) throw new IllegalArgumentException("argumento para Word é null");
+         this.synset = synset;
+         this.gloss = gloss;
+         this.hypernyms = new Bag<Integer>();
+      }
+
+      void add (Integer number) {
+         if (number == null) throw new IllegalArgumentException("argumento para add é null");
+         this.hypernyms.add(number);
+      }
+
+   }
+
 
    // constructor takes the name of the two input files
    public WordNet(String synsets, String hypernyms) {
       In syn;
       In hyper;
+      ST<Integer, Word> st = new ST<Integer, Word>();
+
       try {
          syn = new In(synsets);
          while (!syn.isEmpty()) {
-            String line = in.readLine();
-            System.out.println(line);
+            String line = syn.readLine();
+            String[] words = line.split(",");
+            Word w = new Word(words[1], words[2]);
+            st.put(Integer.valueOf(words[0]), w);   
          }
+        for (Integer s : st.keys())
+            StdOut.println(s + " " + st.get(s).synset);
 
       }
       catch (IllegalArgumentException e) {
@@ -24,15 +54,12 @@ public class WordNet {
       catch (IllegalArgumentException e) {
             System.out.println(e);
         }
-      while (!syn.isEmpty()) {
-
-      }
 
 
    }
 
    // all WordNet nouns
-   public Iterable<String> nouns() {}
+   //public Iterable<String> nouns() {}
 
    // is the word a WordNet noun?
    public boolean isNoun(String word) {
